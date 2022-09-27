8f275e05f807e3c6e1bf17648b6e5300597c6d33
Guillaume Bodson <guillaume.bodson@contraste.com>
2022-09-27 07:03:20 +0200

    Add Product repository and dto

---
+++ Mango.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,49 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _prodctRepository;
+
+        public ProductApiController(IProductRepository prodctRepository)
+        {
+            _prodctRepository = prodctRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                _response.Result = await _prodctRepository.GetProducts();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString()};
+            }
+            return _response;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                _response.Result = await _prodctRepository.GetProductById(id);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
